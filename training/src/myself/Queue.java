b46b3d71b2ebb0e328613426d7b035035bb328fb
won <won@192.168.31.225>
2012-10-19 17:31:45 +0900

    20121019_01

--- training/src/myself/Queue.java
+++ training/src/myself/Queue.java
@@ -1,5 +1,48 @@
 package myself;
 
+import java.lang.reflect.Array;
+
+/*
+Reference URL
+http://www.cs.cmu.edu/~adamchik/15-121/lectures/Stacks%20and%20Queues/Stacks%20and%20Queues.html
+http://www.cs.cmu.edu/~adamchik/15-121/lectures/Stacks%20and%20Queues/code/ArrayQueue.java
+ */
+
 public class Queue {
+	
+	public int queue_length;
+
+	public int getQueue_length() {
+		return queue_length;
+	}
+
+	public void setQueue_length(int queue_length) {
+		this.queue_length = queue_length;
+	}
+	
+	public boolean isEmpty() {
+		return false;
+	}
+	
+	public Array getFront() {
+		return null;
+	}
+	
+	public Array dequeue() {
+		return null;
+	}
+	
+	public void enqueue(Array a) {
+		
+	}
+
+	public Queue() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public void clear() {
+		
+	}
 
 }
